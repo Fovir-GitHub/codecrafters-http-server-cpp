ea2ccd5ac59d5089e82ec6b4b064ef335e721644
Fovir <fovir@disroot.org>
2025-04-04 10:24:08 +0800

    feat: add `HttpMessage` class

---
+++ src/http_message.cpp
@@ -0,0 +1,115 @@
+#include "http_message.h"
+#include <algorithm>
+#include <cctype>
+#include <filesystem>
+#include <iostream>
+#include <locale>
+#include <sstream>
+
+namespace fs = std::filesystem;
+
+std::string HttpMessage::TrimInvisibleCharacters(std::string s)
+{
+    return std::string(s.begin(),
+                       std::find_if_not(s.begin(), s.end(), ::isgraph));
+}
+
+HttpMessage::HttpMessage(const std::string & buffer)
+{
+    ParseRequest(buffer);
+}
+
+void HttpMessage::ParseRequest(const std::string & buffer)
+{
+    std::istringstream iss(buffer);
+    std::string        temp;
+
+    std::getline(iss, temp);
+    request_status_line = RequestStatusLine(temp);
+
+    while (std::getline(iss, temp) && temp != "\r")
+    {
+        size_t colon_position                        = temp.find(':');
+        header_lines[temp.substr(0, colon_position)] = TrimInvisibleCharacters(
+            temp.substr(colon_position + 1, temp.size()));
+    }
+
+    body = std::string(std::istreambuf_iterator<char>(iss),
+                       std::istreambuf_iterator<char>());
+    return;
+}
+
+void HttpMessage::SetResponseStatusLine(const std::string & hv)
+{
+    int status_code = 200;
+
+    if (!fs::exists(fs::current_path().string() + request_status_line.path))
+        status_code = 404;
+
+    response_status_line =
+        ResponseStatusLine(hv, status_code, HTTP_CODE_PHRASE.at(status_code));
+
+    return;
+}
+
+void HttpMessage::SetHeaderLine(const std::string & key,
+                                const std::string & value)
+{
+    try
+    {
+        header_lines.at(key) = value;
+    }
+    catch (const std::out_of_range & e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return;
+}
+
+std::string HttpMessage::MakeResponseStatusLine()
+{
+    return "HTTP/" + response_status_line.http_version + " " +
+           std::to_string(response_status_line.status_code) + " " +
+           response_status_line.option_phrase + "\r\n";
+}
+
+std::string HttpMessage::MakeResponse()
+{
+    SetResponseStatusLine();
+    std::string response = MakeResponseStatusLine();
+
+    SetContentLength();
+    for (const auto & [key, value] : header_lines)
+        response.append(key + ": " + value + "\r\n");
+
+    response.append("\r\n");
+    response.append(body);
+
+    return response;
+}
+
+void HttpMessage::SetContentLength()
+{
+    SetHeaderLine("Content-Length", std::to_string(body.size()));
+}
+
+void HttpMessage::Show()
+{
+}
+
+HttpMessage::RequestStatusLine::RequestStatusLine(const std::string & rsl)
+{
+    std::istringstream iss(rsl);
+    std::string        temp;
+
+    iss >> temp;
+    this->method = HTTP_METHODS.at(temp);
+
+    iss >> temp;
+    this->path = temp;
+
+    iss >> temp;
+    this->http_version = std::string(
+        std::find_if(temp.begin(), temp.end(), ::isdigit), temp.end());
+}
---
+++ src/http_message.h
@@ -0,0 +1,72 @@
+#ifndef _HTTP_MESSAGE_H_
+#define _HTTP_MESSAGE_H_
+
+#include <string>
+#include <unordered_map>
+
+const std::unordered_map<std::string, int> HTTP_METHODS = {
+    {"GET", 0},
+    {"POST", 1},
+};
+
+const std::unordered_map<int, std::string> HTTP_CODE_PHRASE = {
+    {200, "OK"},
+    {404, "Not Found"},
+};
+
+class HttpMessage
+{
+private:
+    struct RequestStatusLine
+    {
+        int         method;
+        std::string path;
+        std::string http_version;
+
+        RequestStatusLine() {}
+        RequestStatusLine(const std::string & rsl);
+    } request_status_line;
+
+    struct ResponseStatusLine
+    {
+        std::string http_version;
+        int         status_code;
+        std::string option_phrase;
+
+        ResponseStatusLine(const std::string & hv = "1.1", int sc = 200,
+                           const std::string & op = "OK")
+            : http_version(hv)
+            , status_code(sc)
+            , option_phrase(op)
+        {
+        }
+    } response_status_line;
+
+    std::unordered_map<std::string, std::string> header_lines;
+    std::string                                  body;
+
+    std::string TrimInvisibleCharacters(std::string s);
+
+public:
+    HttpMessage() {}
+    HttpMessage(const std::string & buffer);
+
+    void ParseRequest(const std::string & buffer);
+
+    void SetResponseStatusLine(const std::string & hv = "1.1");
+
+    void ClearHeader() { header_lines.clear(); }
+
+    void SetHeaderLine(const std::string & key, const std::string & value);
+    void SetBody(const std::string & b) { body = b; }
+
+    std::string MakeResponseStatusLine();
+    std::string MakeResponse();
+
+    int  GetContentLength() const { return body.size(); }
+    void SetContentLength();
+
+    void Show();
+};
+
+#endif // !_HTTP_MESSAGE_H_
