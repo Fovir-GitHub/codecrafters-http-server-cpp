96f9c310b35686ebe1712f676e03e2afc0569af5
Fovir <fovir@disroot.org>
2025-04-05 18:00:22 +0800

    chore: update .gitignore

---
+++ src/server/server.h
@@ -0,0 +1,17 @@
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+#define BEGIN_SERVER_NAMESPACE \
+    namespace server           \
+    {
+#define END_SERVER_NAMESPACE }
+
+BEGIN_SERVER_NAMESPACE
+
+class Server
+{
+};
+
+END_SERVER_NAMESPACE
+
+#endif // !_SERVER_H_
