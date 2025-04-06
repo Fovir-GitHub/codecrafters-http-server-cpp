f92804083e72502b522e9be41dfccac14601102e
Fovir <fovir@disroot.org>
2025-04-06 22:24:52 +0800

    feat: add `Message::Request` class and add `TrimInvisibleCharacters()` method and constructor function

---
+++ src/http/message.cpp
@@ -0,0 +1,41 @@
+#include "message.h"
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+
+std::string
+message::Message::Request::TrimInvisibleCharacters(const std::string & s)
+{
+    return std::string(
+        std::find_if_not(s.begin(), s.end(), ::isspace),
+        std::find_if_not(s.rbegin(), s.rend(), ::isspace).base());
+}
+
+message::Message::Request::Request(const std::string & original_request)
+{
+    std::string        temp;
+    std::istringstream iss(original_request);
+
+    iss >> temp; /* Get the http method */
+    status_line.method = temp;
+
+    iss >> temp; /* Get the http path */
+    status_line.path = temp;
+
+    iss >> temp; /* Get the http version */
+    status_line.http_version = std::string(
+        std::find_if(temp.begin(), temp.end(), ::isdigit), temp.end());
+
+    // Get headers
+    while (std::getline(iss, temp) && temp != "\r")
+    {
+        size_t colon_position = temp.find(':'); /* Find colon's position */
+        header_lines[TrimInvisibleCharacters(temp.substr(0, colon_position))] =
+            TrimInvisibleCharacters(
+                temp.substr(colon_position + 1, temp.size()));
+    }
+
+    // The remain part is body
+    body = std::string(std::istreambuf_iterator<char>(iss),
+                       std::istreambuf_iterator<char>());
+}
--- src/http/message.h
+++ src/http/message.h
@@ -1,6 +1,52 @@
 #ifndef _MESSAGE_H_
 #define _MESSAGE_H_
 
+#include <string>
+#include <unordered_map>
 
+#define BEGIN_MESSAGE_NAMESPACE \
+    namespace message           \
+    {
+#define END_MESSAGE_NAMESPACE }
+
+BEGIN_MESSAGE_NAMESPACE
+
+class Message
+{
+private:
+    class Request
+    {
+    private:
+        struct StatusLine
+        {
+            std::string method;
+            std::string path;
+            std::string http_version;
+
+            StatusLine() {}
+        } status_line;
+
+        std::unordered_map<std::string, std::string> header_lines;
+        std::string                                  body;
+
+        /**
+         *@brief Remove invisible characters from the begin and the end of the
+         *string
+         *
+         * @param s the original string
+         * @return std::string the string after trim
+         */
+        std::string TrimInvisibleCharacters(const std::string & s);
+
+    public:
+        Request(const std::string & original_request);
+    };
+
+    class Response
+    {
+    };
+};
+
+END_MESSAGE_NAMESPACE
 
 #endif // !_MESSAGE_H_
