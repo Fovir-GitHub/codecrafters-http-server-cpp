37be195ec083d23a7b58ca8386221e41470db8ad
Fovir <fovir@disroot.org>
2025-04-07 01:04:17 +0800

    feat: set the path if the program is running with arguments

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <arpa/inet.h>
 #include <cstdlib>
 #include <cstring>
+#include <filesystem>
 #include <iostream>
 #include <netdb.h>
 #include <string>
@@ -10,8 +11,13 @@
 #include <thread>
 #include <unistd.h>
 
+namespace fs = std::filesystem;
+
 int main(int argc, char ** argv)
 {
+    if (argc == 3 && argv[1] == "--directory")
+        fs::current_path(argv[2]);
+
     server::Server http_server(4221);
 
     http_server.InitializeSocket();
